9283a109e49fad1911621b587001b5c20f405832
Petro Zhovnir <zhovnir@gmail.com>
2022-02-03 19:28:46 +0200

    footer

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const swiper = new Swiper('.swiper', {
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper__nav_next',
+      prevEl: '.swiper__nav_prev',
     },
   
     // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // }
   });
