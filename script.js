d6f477521af5f45dbdb6c5346469e22f84e17525
Petro <zhownir@gmail.com>
2022-06-05 21:43:56 +0300

    fixed grid

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+ const swiper = new Swiper('.swiper', {
     // Optional parameters,
     slidesPerView: 1,
     spaceBetween: 30,
@@ -44,12 +44,67 @@ menuCheck.onclick = function()
 
 
 // ================VIDEO==================
- 
-const video_play = document.getElementById("video_play_btn");
+//PLAY/PAUSE
+const videoPlayBtn = document.getElementById("video_play_btn");
+const video = document.querySelector(".video__media");
+const timeline = document.querySelector(".player__timeline");
+const timelineBar = document.querySelector(".timeline-bar");
 
-const video_source = document.getElementsByClassName("video__media");
-
-
-video_play.onclick = function(){
-  video_source.paused = true;
+function togglePlayPause() {
+  if(video.paused){
+    videoPlayBtn.className = "pause";
+    video.play();
+  } else{
+    videoPlayBtn.className = "play";
+    video.pause();
+  }
+}
+videoPlayBtn.onclick = function(){
+  togglePlayPause();
 };
+
+//TIMELINE
+video.addEventListener("timeupdate", function(){
+  let timelinePos = video.currentTime / video.duration;
+  timelineBar.style.width = timelinePos *100 + "%";
+});
+
+//DURATION
+const timeElapsed = document.getElementById("time-elapsed");
+const duration = document.getElementById("duration"); 
+
+function formatTime(timeInSec) {
+  const result = new Date(timeInSec * 1000).toISOString().substr(11, 8);
+
+  return {
+    minutes: result.substr(3, 2),
+    seconds: result.substr(6, 2),
+  };
+};
+
+function initializeVideo() {
+  const videoDuration = Math.round(video.duration);
+  const time = formatTime(videoDuration);
+
+
+  duration.innerText = `${time.minutes}:${time.seconds}`;
+  duration.setAttribute('date-time', `${time.minutes}m ${time.seconds}s`);
+}
+function updateTimeElapsed() {
+  const time = formatTime(Math.round(video.currentTime));
+  timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+  timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+
+
+
+video.addEventListener('loadedmetadata', initializeVideo);
+
+video.addEventListener('timeupdate', updateTimeElapsed);
+
+//TIMELINE CLICK
+timeline.addEventListener('click', (e) =>{
+  const ProgressTime = (e.offsetX/timeline.offsetWidth) * video.duration;
+  video.currentTime = ProgressTime;
+});
