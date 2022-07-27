9377cdc05a1950bcd45cc5763c3ad002d71ac28f
Petro <zhownir@gmail.com>
2022-07-27 15:47:08 +0300

    fix

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
- const swiper = new Swiper('.swiper', {
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+const swiper = new Swiper('.swiper', {
     // Optional parameters,
     slidesPerView: 1,
     spaceBetween: 30,
@@ -76,6 +80,11 @@ videoPlayBtn.onclick = function(){
   togglePlayPause();
 };
 
+
+// document.querySelector('.video').addEventListener('click', function(){
+//   togglePlayPause();
+//   console.log("ffdf");
+// });
 //TIMELINE
 video.addEventListener("timeupdate", function(){
   let timelinePos = video.currentTime / video.duration;
@@ -190,7 +199,8 @@ const trackImageWrapper = document.querySelector('.tracks__img-wrapper');
 
 trackBackLigth(currentTrackNumber);
 
-// audio.volume = 0.1;
+audio.volume = 0.2;
+video.volume= 0.1;
 
 
 const runEq = createEq(audio, canvas, canvasWrapper);
@@ -233,7 +243,9 @@ audioPlayBtn.onclick = function(){
   toggleAudioPlayPause();
 };
 
-
+// audioPlayBtn.addEventListener('touchend', function () {
+//   toggleAudioPlayPause();  
+// });
 
 //DURATION
 
@@ -406,3 +418,28 @@ function trackBackLigth (currentTrackNumber) {
 //   // console.log(element);
 //   element.classList.toggle('backlight');
 // };
+
+
+
+
+//UP BUTTON
+
+//Get the button:
+mybutton = document.getElementById("upScrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
